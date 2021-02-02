2048cc0c5e3044f936406d8899d8b09c860db4a0
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2021-02-02 17:36:59 -0300

    Update metadata.py

--- IPMI_plugin/IPMI_plugin_tensorboard/metadata.py
+++ IPMI_plugin/IPMI_plugin_tensorboard/metadata.py
@@ -1,1 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Standard imports
+from tensorboard.compat.proto import summary_pb2
+
 PLUGIN_NAME = "IMPI_plugin"
+
+
+def CreateSummaryMetadata(description=None):
+  return summary_pb2.SummaryMetadata(
+      summary_description=description,
+      plugin_data=summary_pb2.SummaryMetadata.PluginData(
+          plugin_name=PLUGIN_NAME))
