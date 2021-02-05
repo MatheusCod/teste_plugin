9277d337063bf3a5e14ff372e24d92949d24252c
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2021-02-05 11:30:59 -0300

    Update plugin.py

--- IPMI_plugin/IPMI_plugin_tensorboard/plugin.py
+++ IPMI_plugin/IPMI_plugin_tensorboard/plugin.py
@@ -1,4 +1,4 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,134 +12,126 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""TensorBoard Plugin abstract base class.
+"""A sample plugin to demonstrate dynamic loading."""
+########## NEW IMPORT ##########
+import random
+########## NEW IMPORT ##########
 
-Every plugin in TensorBoard must extend and implement the abstract
-methods of this base class.
-"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-
-import mimetypes
-import os
 import json
-from werkzeug import wrappers
-import tensorflow.compat.v2 as tf
+import os
 
-from tensorboard import errors
-from tensorboard.backend import http_util
 from tensorboard.plugins import base_plugin
 from tensorboard.util import tensor_util
-from .metadata import PLUGIN_NAME
+import werkzeug
+from werkzeug import wrappers
 
-#dados obtidos estara nesse diretorio
-_PLUGIN_DIRECTORY_PATH_PART = "/data/plugin/IPMI_plugin/"
+from tensorboard_plugin_example import metadata
 
-class IPMI_Plugin(base_plugin.TBPlugin):
-    """Using IPMI in TensorBoard to be abble to debbugin POWER in process."""
-
-    plugin_name = PLUGIN_NAME
+class ExamplePlugin(base_plugin.TBPlugin):
+    plugin_name = metadata.PLUGIN_NAME
 
     def __init__(self, context):
-        """Instantiates ExampleRawScalarsPlugin.
+        """Instantiates ExamplePlugin.
+
         Args:
-          context: A base_plugin.TBContext instance.
+        context: A base_plugin.TBContext instance.
         """
         self._multiplexer = context.multiplexer
-    
-    def get_plugin_apps(self):
-        return {
-            "/scalars": self.scalars_route,
-            "/tags": self._serve_tags,
-            "/static/*": self._serve_static_file,
-        }
-    
-    
-    
-    @wrappers.Request.application
-    def _serve_tags(self, request):
-        """Serves run to tag info.
-        Frontend clients can use the Multiplexer's run+tag structure to request data
-        for a specific run+tag. Responds with a map of the form:
-        {runName: [tagName, tagName, ...]}
-        """
-        run_tag_mapping = self._multiplexer.PluginRunToTagToContent(
-            self.plugin_name
-        )
-        run_info = {run: list(tags) for (run, tags) in run_tag_mapping.items()}
 
-        return http_util.Respond(request, run_info, "application/json")
-
-    @wrappers.Request.application
-    def _serve_static_file(self, request):
-       
-        static_path_part = request.path[len(_PLUGIN_DIRECTORY_PATH_PART) :]
-        resource_name = os.path.normpath(
-            os.path.join(*static_path_part.split("/"))
-        )
-        if not resource_name.startswith("static" + os.path.sep):
-            return http_util.Respond(
-                request, "Not found", "text/plain", code=404
-            )
-
-        resource_path = os.path.join(os.path.dirname(__file__), resource_name)
-        with open(resource_path, "rb") as read_file:
-            mimetype = mimetypes.guess_type(resource_path)[0]
-            return http_util.Respond(
-                request, read_file.read(), content_type=mimetype
-            )
     def is_active(self):
         """Returns whether there is relevant data for the plugin to process.
-        When there are no runs with scalar data, TensorBoard will hide the plugin
-        from the main navigation bar.
+
+        When there are no runs with greeting data, TensorBoard will hide the
+        plugin from the main navigation bar.
         """
         return bool(
-            self._multiplexer.PluginRunToTagToContent(self.plugin_name)
+            self._multiplexer.PluginRunToTagToContent(metadata.PLUGIN_NAME)
         )
 
-    def frontend_metadata(self):
-        return base_plugin.FrontendMetadata(
-            es_module_path="/index.js",
-            tab_name='IPMI_Plugin',
-            remove_dom=False,
-            element_name=None)
+    def get_plugin_apps(self):
+        return {
+            "/index.js": self._serve_js,
+            "/printdata": self._serve_printdata,
+            "/plotgraph": self._serve_plotgraph,
+            "/tags": self._serve_tags,
+            "/greetings": self._serve_greetings
+        }
 
-    def scalars_impl(self, tag, run):
-        """Returns scalar data for the specified tag and run.
-        For details on how to use tags and runs, see
-        https://github.com/tensorflow/tensorboard#tags-giving-names-to-data
-        Args:
-          tag: string
-          run: string
-        Returns:
-          A list of ScalarEvents - tuples containing 3 numbers describing entries in
-          the data series.
-        Raises:
-          errors.NotFoundError: if run+tag pair has no scalar data.
-        """
-        try:
-            tensor_events = self._multiplexer.Tensors(run, tag)
-            values = [
-                (
-                    tensor_event.wall_time,
-                    tensor_event.step,
-                    tensor_util.make_ndarray(tensor_event.tensor_proto).item(),
-                )
-                for tensor_event in tensor_events
-            ]
-        except KeyError:
-            raise errors.NotFoundError(
-                "No scalar data for run=%r, tag=%r" % (run, tag)
-            )
-        return values
+    def frontend_metadata(self):
+        return base_plugin.FrontendMetadata(es_module_path="/index.js")
+    
+    @wrappers.Request.application
+    def _serve_js(self, request):
+        del request  # unused
+        filepath = os.path.join(os.path.dirname(__file__), "static", "index.js")
+        with open(filepath) as infile:
+            contents = infile.read()
+        return werkzeug.Response(
+            contents, content_type="application/javascript"
+        )
+    
+    @wrappers.Request.application
+    def _serve_printdata(self, request):
+        del request
+        new_dict = {'Second': 'Print'}
+        contents = json.dumps(new_dict)
+        return werkzeug.Response(contents, content_type="application/json")
+    
+    @wrappers.Request.application
+    def _serve_plotgraph(self, request):
+        del request
+        size = random.randint(10, 15)
+        new_dict = {
+            "x": [i for i in range(size)],
+            "y": [random.randint(0, 20) for i in range(size)]
+        }
+        contents = json.dumps(new_dict)
+        return werkzeug.Response(contents, content_type="application/json")
 
     @wrappers.Request.application
-    def scalars_route(self, request):
-        """Given a tag and single run, return array of ScalarEvents."""
-        tag = request.args.get("tag")
+    def _serve_tags(self, request):
+        del request  # unused
+        mapping = self._multiplexer.PluginRunToTagToContent(
+            metadata.PLUGIN_NAME
+        )
+        result = {run: {} for run in self._multiplexer.Runs()}
+        for (run, tag_to_content) in mapping.items():
+            for tag in tag_to_content:
+                summary_metadata = self._multiplexer.SummaryMetadata(run, tag)
+                result[run][tag] = {
+                    "description": summary_metadata.summary_description,
+                }
+        contents = json.dumps(result, sort_keys=True)
+        with open('out.txt', 'w') as f:
+            print("mapping:", file=f)
+            print(mapping, file=f)
+            print()
+            print("result:", file=f)
+            print(result, file=f)
+            print()
+            print("contents:", file=f)
+            print(contents, file=f)
+        return werkzeug.Response(contents, content_type="application/json")
+
+    @wrappers.Request.application
+    def _serve_greetings(self, request):
+        """Serves greeting data for the specified tag and run.
+
+        For details on how to use tags and runs, see
+        https://github.com/tensorflow/tensorboard#tags-giving-names-to-data
+        """
         run = request.args.get("run")
-        body = self.scalars_impl(tag, run)
-        return http_util.Respond(request, body, "application/json")
+        tag = request.args.get("tag")
+        if run is None or tag is None:
+            raise werkzeug.exceptions.BadRequest("Must specify run and tag")
+        try:
+            data = [
+                tensor_util.make_ndarray(event.tensor_proto)
+                .item()
+                .decode("utf-8")
+                for event in self._multiplexer.Tensors(run, tag)
+            ]
+        except KeyError:
+            raise werkzeug.exceptions.BadRequest("Invalid run or tag")
+        contents = json.dumps(data, sort_keys=True)
+        return werkzeug.Response(contents, content_type="application/json")
